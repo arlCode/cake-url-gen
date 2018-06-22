a887cb1e1cc0668d334c605b0865568b69db4a67
Adam Lewis <lewis.adam1992@gmail.com>
2018-06-22 16:36:01 -0700

    Prepping for AJAX calls.

--- App_Start/controller.php
+++ App_Start/controller.php
@@ -10,6 +10,8 @@ $api->getAPIResponse(); // Get the API Response call
 $jordanCake = $api->createArray(null, "data", "campaign", "cake", 3); // Creates an Array out of JSON to parse data.
 
 
+// Create PHP call to DB to grab the Campaign ID and Offer ID
+
 
 ?>
 
