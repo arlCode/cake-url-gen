b02036faf4ed0d3e5b7a9fc3e1c6b6f57c693ed2
Adam Lewis <lewis.adam1992@gmail.com>
2018-06-22 12:24:37 -0700

    Completed the campaign functionality.

--- js/eventHandling.js
+++ js/eventHandling.js
@@ -1,4 +1,4 @@
-var urlString = "https://example.com/?aff=&src=&c=213&utm_medium=${CAMPAIGN_ID}"; //window.location.href
+var urlString = "https://example.com/?aff=&src=&c=&utm_medium=${CAMPAIGN_ID}"; //window.location.href
 var url = new URL(urlString);
 // var c = url.searchParams.get("c");
 
@@ -7,26 +7,33 @@ $(".campaign").on("click", function(){ // Changes the parameter values per click
 
     if($(this).text() == " VA Benefits - VeteranLending.com "){
 
-        replaceUrlParam(urlString, "aff", 250);
+        console.log(replaceUrlParam(urlString, "aff", 250));
 
     } else {
 
-
-
-        replaceUrlParam(url, "aff", 3);
+        console.log(replaceUrlParam(urlString, "aff", 3));
 
     }
 
-    return $("#url-output").val(url);
+    return $("#url-output").val(urlString);
 });
 
 
-$(".traffic-source").on("click", function() { // Changes the parameter values per click.
+$(".traffic-source").on("click", function(){ // Changes the parameter values per click.
 
 
+    if($(this).text() == " VA Benefits - VeteranLending.com "){
 
-    $("#url-output").val("?aff=3&src=&c=&utm_medium=${CAMPAIGN_ID}");
-})
+        console.log(replaceUrlParam(urlString, "src", 250));
+
+    } else {
+
+        console.log(replaceUrlParam(urlString, "src", 3));
+
+    }
+
+    return $("#url-output").val(urlString);
+});
 
 
 
