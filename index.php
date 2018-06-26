b6577abf1fed93ffcf09305a0fd44ad32f154316
Adam Lewis <lewis.adam1992@gmail.com>
2018-06-26 15:21:55 -0700

    Rough UI design.

--- index.php
+++ index.php
@@ -8,96 +8,111 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 ?>
-<head>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
-        crossorigin="anonymous">
-    
-</head>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<?php require('./app_start/controller.php'); ?>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+            crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
 
-<body>
+    <?php require('./app_start/controller.php'); ?>
 
-<!-- Navigation Placeholder -->
+    <body>
 
-    <main role="main" class="container">
+        <!-- Navigation Placeholder -->
 
-        <div class="jumbotron">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            Select Traffic Source
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <?php foreach($api->sqlOutput("traffic_source") as $tSource) { ?>
-                                
-                                <a class="dropdown-item traffic-source" href="#"><?php echo $tSource  ?></a>
-                           
-                            <?php } ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            Select Campaign
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <?php
-                                foreach($jordanCake as $campaign) { ?>
-                                    
-                                    <a class="dropdown-item campaign" href="#"> <?php echo $campaign["offer_name"]; ?> </a>
+        <main role="main" id="main" class="container">
 
-                            <?php } ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            Select Landing Page
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10">
+                        <button class="btn btn-success float-right">Upload New Lander</button>
                     </div>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="input-group">
-                                <input id="url-output" disabled type="text" class="form-control" value="">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-secondary btn-primary" type="button">Go!</button>
-                                </span>
+            <div class="flex-container">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                Select Traffic Source
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <?php foreach($api->sqlOutput("traffic_source") as $tSource) { ?>
+
+                                <a class="dropdown-item traffic-source" href="#">
+                                    <?php echo $tSource  ?>
+                                </a>
+
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                Select Campaign
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <?php
+                                foreach($jordanCake as $campaign) { ?>
+
+                                    <a class="dropdown-item campaign" href="#">
+                                        <?php echo $campaign["offer_name"]; ?> </a>
+
+                                    <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                Select Landing Page
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
-<footer>
-</footer>
 
-<script src="./js/dataHandling.js"></script>
-<script src="./js/eventHandling.js"></script>
-<script src="./js/stackoverflow.js"></script> <!-- Because I don't have all the answers. -->
-</body>
+            <div class="flex-container">
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="input-group">
+                                <input id="url-output" disabled type="text" class="form-control" value="">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-secondary btn-primary" type="button">Copy</button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </main>
+        <footer>
+        </footer>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+            crossorigin="anonymous"></script>
+        <script src="./js/dataHandling.js"></script>
+        <script src="./js/eventHandling.js"></script>
+        <script src="./js/stackoverflow.js"></script>
+        <!-- Because I don't have all the answers. -->
+    </body>
+
 </html>
