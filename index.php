cb68ad011a8d4e68e7234b430b50492c998624be
Adam Lewis <lewis.adam1992@gmail.com>
2018-06-29 11:16:38 -0700

    Changed the styling for the thumbnail display.

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ error_reporting(E_ALL);
                             </div>
                         </div>
 
-                        <img id="thumbnail-display" class="img-fluid img-thumbnail" />  <!-- Image responsive dynamic lander thumbnails. -->
+                        <img id="thumbnail-display" />  <!-- Image responsive dynamic lander thumbnails. -->
                     </div>
                 </div>
             </div>
--- js/eventHandling.js
+++ js/eventHandling.js
@@ -1,4 +1,4 @@
-$(".campaign").on("click", function(){ // Changes the parameter values per click.
+$(".campaign").on("click", function () { // Changes the parameter values per click.
 
 
     // Uncomment the if statement if you need the secondary change.
@@ -8,10 +8,10 @@ $(".campaign").on("click", function(){ // Changes the parameter values per click
 
     // } else {
 
-        urlString.updateUrl = replaceUrlParam(urlString.currentString, "aff", 3);
-        
-        // Number 3 affiliate ID is Jordans, this will be pulled in
-        // a different method once moved into server.
+    urlString.updateUrl = replaceUrlParam(urlString.currentString, "aff", 3);
+
+    // Number 3 affiliate ID is Jordans, this will be pulled in
+    // a different method once moved into server.
 
     // }
 
@@ -19,7 +19,7 @@ $(".campaign").on("click", function(){ // Changes the parameter values per click
 });
 
 
-$(".traffic-source").on("click", function(){ // Changes the parameter values per click.
+$(".traffic-source").on("click", function () { // Changes the parameter values per click.
 
 
     urlString.updateUrl = replaceUrlParam(urlString.currentString, "src", urlString.getFirstCharacter($(this)) + "-" + 101);
@@ -29,22 +29,19 @@ $(".traffic-source").on("click", function(){ // Changes the parameter values per
 });
 
 
-$(".lander").on("click", function() {
+$(".lander").on("click", function () {
 
 
-    
+
 
     var newUrl = "https://google.com/", // TODO: Get this from the lander data row.
-    reg = /(.+)\?/; // Regex everything before the question mark.
-    
+        reg = /(.+)\?/; // Regex everything before the question mark.
+
     urlString.updateUrl = urlString.currentString.replace(reg, newUrl + "?"); // Update the URL in the urlString object.
 
-    
+
     $("#url-output").val(urlString.currentString); // URL Output
     $("#thumbnail-display").attr("src", "thumbs/default.jpg");
 
     console.log("Display lander screenshot stored in DB.");
 })
-
-
-
